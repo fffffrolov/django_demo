2ef939aae05d4eda8653da39c442ac800a62337a
fffffrolov <frolowroman@gmail.com>
2020-12-15 21:21:22 +0700

    🫀

--- demo.sh
+++ demo.sh
@@ -2,6 +2,6 @@
 cp ./src/.env.templ ./src/.env \
   && docker-compose build \
   && docker-compose up -d \
-  && sleep 5 \
+  && sleep 6 \
   && python -mwebbrowser http://localhost:8000/api/v1/docs/ \
   && python -mwebbrowser http://localhost:8000/admin/
