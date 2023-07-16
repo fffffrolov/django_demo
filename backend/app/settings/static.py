ea7fa5d7254c1893f6340689ee974854d46ab7d8
Roman Hahn <roman@frolov.me>
2023-07-16 21:46:55 +0200

    fix static

--- backend/app/settings/static.py
+++ backend/app/settings/static.py
@@ -4,5 +4,5 @@ DEFAULT_STATIC_ROOT = (BASE_PATH.parent / 'static').resolve()
 STATIC_URL = 'static/'
 STATIC_ROOT = env('STATIC_ROOT', cast=str, default=str(DEFAULT_STATIC_ROOT))
 STATICFILES_DIRS = [
-    BASE_PATH / 'static',
+    BASE_PATH / 'static_src',
 ]
